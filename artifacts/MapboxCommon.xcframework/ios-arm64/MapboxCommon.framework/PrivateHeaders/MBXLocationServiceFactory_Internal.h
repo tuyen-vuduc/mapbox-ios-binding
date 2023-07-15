dc2117bde618457689cb28265464468bc2c27941
Tuyen Vu <hi@tuyen-vuduc.tech>
2023-07-15 17:38:05 +0700

    - add github publish-nuget workflow - remove unneccessary artifacts

--- artifacts/MapboxCommon.xcframework/ios-arm64/MapboxCommon.framework/PrivateHeaders/MBXLocationServiceFactory_Internal.h
+++
@@ -1,8 +0,0 @@
-// This file is generated and will be overwritten automatically.
-
-#import "MBXLocationServiceFactory.h"
-
-@interface MBXLocationServiceFactory ()
-+ (void)setUserDefinedForCustom:(nonnull id<MBXLocationService>)custom;
-+ (nonnull id<MBXLocationService>)locationService __attribute((ns_returns_retained));
-@end
--- artifacts/MapboxMapObjC.xcframework/ios-arm64_x86_64-simulator/MapboxMapObjC.framework/Headers/MapboxMapObjC.h
+++
@@ -1,7 +0,0 @@
-#import <Foundation/Foundation.h>
-
-//! Project version number for MapboxMapObjC.
-FOUNDATION_EXPORT double MapboxMapObjCVersionNumber;
-
-//! Project version string for MapboxMapObjC.
-FOUNDATION_EXPORT const unsigned char MapboxMapObjCVersionString[];
